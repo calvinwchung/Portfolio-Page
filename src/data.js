e3725036888ed4fb92be1c978bdb062381d3e00d
calvin chung <calvin.w.chung@gmail.com>
2022-08-23 12:45:48 -0400

    adding links to projects

--- src/data.js
+++ src/data.js
@@ -1,43 +1,41 @@
-import Bolivian from '../src/img/bolivian.png';
+import Bolivian from '../src/img/bolivianlumber.gif';
 import Anti from '../src/img/anti.png';
-import About from '../src/img/about.png';
 import Nucamp from '../src/img/nucampproject.gif';
 import Imposter from '../src/img/imposter.gif';
-import Snake from '../src/img/snake.gif';
-import FirstProf from '../src/img/firstprof.gif';
 import Flappy from '../src/img/flappybird.gif';
+import PickleBall from '../src/img/pickleballapp.gif';
 
 
 export const products = [
   {
     id: 1,
-    img: Anti,
-    link: "http://lama.dev",
+    img: PickleBall,
+    link: "https://play.google.com/store/apps/details?id=com.pickleBallJunkies",
   },
   {
     id: 2,
     img: Bolivian,
-    link: "http://lama.dev",
+    link: "https://zesty-travesseiro-61dd44.netlify.app",
   },
   {
     id: 3,
-    img: FirstProf,
-    link: "http://lama.dev",
+    img: Nucamp,
+    link: "https://www.nucamp.co/",
   },
   {
     id: 4,
-    img: Nucamp,
-    link: "http://lama.dev",
+    img: Anti,
+    link: "https://lively-sfogliatella-f6a97d.netlify.app",
   },
   {
     id: 5,
     img: Imposter,
-    link: "http://lama.dev",
+    link: "https://luminous-manatee-16f1c3.netlify.app",
   },
   {
     id: 6,
     img: Flappy,
-    link: "http://lama.dev",
+    link: "https://neon-khapse-8d324e.netlify.app",
   },
 ];
 
